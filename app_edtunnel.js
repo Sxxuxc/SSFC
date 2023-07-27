f961b03f441d4d8f5a0baec4e11bd1b3324cfaba
Sxxuxc <81020902+Sxxuxc@users.noreply.github.com>
2023-07-28 06:29:40 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '50856b9f-a72d-4c8e-bc16-5231b8419847';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
